1d5c4bc3d363f58ce790fcf8176e6babaae2733b
Liron Yahdav <liron@handl.it>
2014-03-27 16:39:07 -0700

    Initial Commit

---
+++ MyApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MyApp
+//
+//  Created by Liron Yahdav on 3/27/14.
+//  Copyright (c) 2014 Handle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
